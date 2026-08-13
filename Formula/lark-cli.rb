ea219d69c81163da687ca8d95d21460bfc2badf8
hhh2210 <hzy2210@gmail.com>
2026-08-13 12:55:45 +0800

    Update lark-cli to 1.0.86

--- Formula/lark-cli.rb
+++ Formula/lark-cli.rb
@@ -1,7 +1,7 @@
 class LarkCli < Formula
   desc "Lark/Feishu CLI tool"
   homepage "https://github.com/larksuite/cli"
-  version "1.0.65"
+  version "1.0.86"
   license "MIT"
 
   livecheck do
@@ -11,19 +11,19 @@ class LarkCli < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/larksuite/cli/releases/download/v1.0.65/lark-cli-1.0.65-darwin-arm64.tar.gz"
-      sha256 "9135e0412cf6bcb0ce6e6de3308ba878f6f16a887af46c806bdaa17d7d86e768"
+      url "https://github.com/larksuite/cli/releases/download/v1.0.86/lark-cli-1.0.86-darwin-arm64.tar.gz"
+      sha256 "b3f1ddd38c46a5397c7ae5f1ad6fcb8c8c2d76736d89587eda03bf5706c8571c"
     else
-      url "https://github.com/larksuite/cli/releases/download/v1.0.65/lark-cli-1.0.65-darwin-amd64.tar.gz"
-      sha256 "7d8a4539ade2b1bda46936ceae2a73e42a414e444a75b9e2e0f39294b8e61b07"
+      url "https://github.com/larksuite/cli/releases/download/v1.0.86/lark-cli-1.0.86-darwin-amd64.tar.gz"
+      sha256 "8f4055e93bc2469388f0c52cefa5c26a27fbd056cb15d73f2092801e2c5055dc"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/larksuite/cli/releases/download/v1.0.65/lark-cli-1.0.65-linux-arm64.tar.gz"
-      sha256 "f3f11a2e163b2ea9698ae4c5f923a4fbca28274f44cd0a4689bf7588f229242e"
+      url "https://github.com/larksuite/cli/releases/download/v1.0.86/lark-cli-1.0.86-linux-arm64.tar.gz"
+      sha256 "fcea7fef8c4b8f7a144beaa388049c2ff43685cecdcd5a2adf550a9eff1d131d"
     else
-      url "https://github.com/larksuite/cli/releases/download/v1.0.65/lark-cli-1.0.65-linux-amd64.tar.gz"
-      sha256 "2d8fbd33e79d06efcd7243971d3a4e1a049ad91d04f0ca97214c6730e10c24c8"
+      url "https://github.com/larksuite/cli/releases/download/v1.0.86/lark-cli-1.0.86-linux-amd64.tar.gz"
+      sha256 "91d05829c78bfb77eec46d89449aa2786e81f55f7a99b9788ee5d68d62727cfa"
     end
   end
 
